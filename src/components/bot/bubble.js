e5ce15994b4196a4502cb9229f8ace84ffcd9209
Zhenting <zhentzhou@gmail.com>
2017-05-15 16:11:42 -0400

    typist

--- src/components/bot/bubble.js
+++ src/components/bot/bubble.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import Typist from 'react-typist';
+// import TypistLoop from 'react-typist-loop';
 
 export default class Bubble extends Component {
   constructor(props) {
     super(props);
-    this.state = { msg: [0,0,0] };
+    this.state = {
+      msg: [0],
+      text: ['...',"Hello Human"]
+     };
   }
   componentWillMount() {
     let order = this.state.msg;
@@ -11,14 +16,22 @@ export default class Bubble extends Component {
     let time = 0;
     let timeInterval = setInterval( () => {
       time++;
-      if (time%3===0) {
+      if (time%5===0) {
         order.push(0);
         this.setState({ msg: order })
         console.log("count time...", this.state);
       }
-    }, 3000);
+    }, 5000);
+
   }
 
+  // <TypistLoop interval={1000}>
+  //   {[
+  //     'Hello World',
+  //     'Good Morning',
+  //     'Bye',
+  //   ].map(text => <Typist key={text} startDelay={1000}>{text}</Typist>)}
+  // </TypistLoop>
   render() {
     const blockMsg = this.state.msg;
     return (
@@ -26,7 +39,9 @@ export default class Bubble extends Component {
         {
           blockMsg.map((i) =>
             <div className="block">
-              <div className="bot-bubble ">...</div>
+              <div className="bot-bubble">
+                <Typist className="Typist" startDelay={1000}>...</Typist>
+              </div>
             </div>
           )
         }
